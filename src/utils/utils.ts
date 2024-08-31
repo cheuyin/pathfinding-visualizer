0bed3a88ab1aaae9abdc0129336a6a0e60f5bc52
Stanley Cheung <yinstanleycheung@gmail.com>
2024-08-31 10:59:35 -0700

    Implement the FIsher-Yates shuffle algorithm

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,18 @@ export function assert(condition: any, msg?: string): asserts condition is true 
 export const compareCoord = (coord1: Coord, coord2: Coord) => {
   return coord1.x === coord2.x && coord1.y === coord2.y;
 };
+
+// The Fisher-Yates shuffle algorithm.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const shuffle = (arr: any[]) => {
+  let m = arr.length;
+
+  while (m) {
+    const i = Math.floor(Math.random() * m);
+    m--;
+
+    const temp = arr[i];
+    arr[i] = arr[m];
+    arr[m] = temp;
+  }
+};
