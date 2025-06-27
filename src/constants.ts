6033b795ad80c30dc1b295c966f55b63d27c1dd7
cheuyin <yinstanleycheung@gmail.com>
2025-06-27 12:02:19 -0700

    Make grid smaller and animation faster

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 // Centralised constants for UI and animation timings
 
 // Grid
-export const CELL_SIZE_PX = 25;
+export const CELL_SIZE_PX = 20;
 
 // Animation delays (milliseconds)
-export const VISITED_NODE_DELAY_MS = 2;
-export const PATH_NODE_DELAY_MS = 20;
+export const VISITED_NODE_DELAY_MS = 1;
+export const PATH_NODE_DELAY_MS = 10;
 export const MAZE_WALL_DELAY_MS = 1;
